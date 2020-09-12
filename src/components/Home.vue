5f9d978534dd389c3e19d9fa9144972076834e95
J0k3r-K <john2000sy@163.com>
2020-09-12 15:18:42 +0800

    完成登录区域

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,21 +1,19 @@
 <template>
   <div>
-    <el-button type="info" @click="layout">退出</el-button>
+    <el-button type="info" @click="logout">退出</el-button>
   </div>
 </template>
 
 <script>
 export default {
   methods: {
-    layout() {
+    logout() {
       window.sessionStorage.clear()
       this.$router.push('/login')
-      this.$message.success('退出成功')
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
-
 </style>
